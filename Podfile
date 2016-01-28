ef8555039148645b48aa59cf2cd51bdd5477e5d2
Ben Asher <basher@yelp.com>
2016-01-27 19:06:13 -0500

    initial commit

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '8.0'
+use_frameworks!
+
+abstract_target 'SharedDeps' do
+  pod 'AFNetworking/NSURLConnection'
+
+  target 'PodsNonTrivalDepsTest' do
+    pod 'AFNetworking'
+    pod 'AFOAuth2Manager'
+  end
+
+end
+
---
+++ PodsNonTrivalDepsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodsNonTrivalDepsTest
+//
+//  Created by Ben Asher on 1/27/16.
+//  Copyright © 2016 Example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
